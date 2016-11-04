baf5f7944943876c210d863ced46c3d9a8822d44
stephrpi <stephanie@raspberrypi.org>
2016-11-04 11:30:20 +0000

    final attempt

---
+++ 3.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+R = 255
+G = 0
+B = 124
+
+turtle.color((R, G, B))
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
---
+++ turtle 2.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+R = 255
+G = 255
+B = 0
+
+screen.bgcolor((R, G, B))
